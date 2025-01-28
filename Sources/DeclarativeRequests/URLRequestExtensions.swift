0031a382c446e90b5f0ef0e2e14d0bcdc4ceb8a2
Lazar Otasevic <>
2025-01-29 03:07:41 +0400

    clean

--- Sources/DeclarativeRequests/URLRequestExtensions.swift
+++ Sources/DeclarativeRequests/URLRequestExtensions.swift
@@ -1,12 +1,6 @@
 import Foundation
 
 extension URLRequest {
-    static var initial: URLRequest {
-        var res = URLRequest(url: URL(fileURLWithPath: ""))
-        res.url = nil
-        return res
-    }
-
     init(@RequestBuilder builder: () -> RequestTransformer) throws {
         var state = RequestBuilderState()
         let transformer = builder()
