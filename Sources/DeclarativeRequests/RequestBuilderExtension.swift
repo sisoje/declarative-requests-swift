5bfda7ee325f7afacaa1a22cb72ffa0a6b95f123
Mirko Tomic <mirko@m-quart.com>
2025-01-29 13:44:56 +0100

    Add support for concrete types in Request Builder

---
+++ Sources/DeclarativeRequests/RequestBuilderExtension.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public extension RequestBuilder {
+    static func buildExpression(_ url: URL?) -> RootNode {
+        RootNode(BaseURL(url).transformer)
+    }
+
+    static func buildExpression(_ callback: @escaping StateTransformer) -> RootNode {
+        RootNode(callback)
+    }
+
+    static func buildExpression(_ data: Data?) -> RootNode {
+        RootNode { result in
+            result.request.httpBody = data
+        }
+    }
+
+    static func buildExpression(_ path: String) -> RootNode {
+        RootNode(Endpoint(path).transformer)
+    }
+
+    static func buildExpression(_ query: URLQueryItem) -> RootNode {
+        RootNode(Query([query.name: query.value]).transformer)
+    }
+}
