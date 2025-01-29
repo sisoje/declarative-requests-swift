d3db1c5d3b9a2bc2012d6deb36e2a3791505d2b9
Mirko Tomic <mirko@m-quart.com>
2025-01-29 23:42:32 +0100

    Allow custom string Data

--- Sources/DeclarativeRequests/Blocks/DataBody.swift
+++ Sources/DeclarativeRequests/Blocks/DataBody.swift
@@ -1,14 +1,37 @@
 import Foundation
 
 public struct DataBody: CompositeNode {
+    private enum Source {
+        case string(String)
+        case data(Data)
+        case file(URL)
+    }
+    
     public init(_ value: Data) {
-        self.value = value
+        self.source = .data(value)
     }
 
-    let value: Data
-
+    public init(_ value: String) {
+        self.source = .string(value)
+    }
+    
+    public init(fileURL: URL) {
+        self.source = .file(fileURL)
+    }
+    
+    private let source: Source
+    
     public var body: some BuilderNode {
-        RequestState[\.request.httpBody] { value }
-        Header.contentType.addValue("application/octet-stream")
+        RootNode { state in
+            switch source {
+            case .string(let value):
+                state.request.httpBody = value.data(using: .utf8)
+            case .data(let value):
+                state.request.httpBody = value
+            case .file(let url):
+                state.request.httpBodyStream = InputStream(url: url)
+            }
+            state.request.setValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
+        }
     }
 }
