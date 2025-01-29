6a6deaf54ddd6c1dc44a028f430a0e1be9614f24
Mirko Tomic <mirko@m-quart.com>
2025-01-29 14:17:41 +0100

    Add data test

--- Sources/DeclarativeRequests/URLExtensions.swift
+++ Sources/DeclarativeRequests/URLExtensions.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public extension URL {
-    func buildRequest(@RequestBuilder builder: () -> BuilderNode) throws -> URLRequest {
+    func buildRequest(@RequestBuilder _ builder: () -> BuilderNode) throws -> URLRequest {
         try URLRequest {
             builder()
             BaseURL(self)
--- Tests/DeclarativeRequestsTests/DeclarativeRequestsTests.swift
+++ Tests/DeclarativeRequestsTests/DeclarativeRequestsTests.swift
@@ -93,3 +93,17 @@ import Testing
     try builder.transformer(&source)
     #expect(source.request.url?.absoluteString == "https://google.com/getLanguage?count=1")
 }
+
+@Test func concreteTypesDataTest() throws {
+    let testData: [UInt8] = [0x68, 0x65, 0x6C, 0x6C, 0x6F]
+
+    let builder = RootNode {
+        Data(testData)
+    }
+
+    let request = try URL(filePath: "/api/users").buildRequest {
+        builder
+    }
+
+    #expect(request.httpBody == Data(testData))
+}
