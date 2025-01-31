fd562f1ae4125d501c49036b281cc8171b1f5d2f
Mirko Tomic <mirko@m-quart.com>
2025-01-31 18:36:46 +0100

    Update VaporTest.swift

--- Tests/VaporTests/VaporTest.swift
+++ Tests/VaporTests/VaporTest.swift
@@ -4,13 +4,15 @@ import Testing
 import Vapor
 
 @Suite("Multipart Request Tests")
-class MultipartTests {
+class MultipartTests: @unchecked Sendable {
     var app: Application!
+    var receivedRequest: Request?
     
     init() throws {
         app = Application(.testing)
         
-        app.get("upload") { req -> String in
+        app.get("upload") { [weak self] req -> String in
+            self?.receivedRequest = req
             return "Success"
         }
         
@@ -25,5 +27,7 @@ class MultipartTests {
     func testMultipartUpload() async throws {
         let response = try await URLSession.shared.data(from: URL(string: "http://localhost:8080/upload")!)
         #expect(response.0 == "Success".data(using: .utf8))
+        
+        // TODO: check receivedRequest
     }
 }
