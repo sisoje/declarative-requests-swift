a1b9512f5bb80f499154f081350c8af85b3b0ca6
Lazar Otasevic <>
2025-02-02 13:16:30 +0400

    data and string

--- Sources/DeclarativeRequests/Blocks/Authorization.swift
+++ Sources/DeclarativeRequests/Blocks/Authorization.swift
@@ -6,9 +6,12 @@ public enum Authorization {
     }
 
     public static func bearer(_ data: Data) -> some BuilderNode {
+        bearer(data.base64EncodedString())
+    }
+    
+    public static func bearer(_ string: String) -> some BuilderNode {
         custom {
-            let base64 = data.base64EncodedString()
-            return "Bearer \(base64)"
+            return "Bearer \(string)"
         }
     }
 
