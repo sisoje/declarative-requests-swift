4b58b9e543aea4127b196beb1faa23af046bd41b
Mirko Tomic <mirko@m-quart.com>
2025-02-03 09:54:58 +0100

    Decouple vapor request saving into middleware

--- Tests/VaporTests/MockServer.swift
+++ Tests/VaporTests/MockServer.swift
@@ -2,21 +2,17 @@ import Foundation
 import Vapor
 
 actor MockServer {
-    private let vaporRequestHeader = "X-Vapor-Test-ID"
     private let app: Application
-    private var vaporRequests: [String: Request] = [:]
+    private var middleware: MockServerMiddleware
 
     init() {
         app = Application(.testing)
+        middleware = MockServerMiddleware()
+        app.middleware.use(middleware)
         app.http.server.configuration.port = .zero
         app.get(.catchall, use: requestHandler)
         app.post(.catchall, use: requestHandler)
         try! app.start()
-    }
-
-    func getVaporRequest(_ response: URLResponse) -> Request {
-        let id = (response as! HTTPURLResponse).value(forHTTPHeaderField: vaporRequestHeader)!
-        return vaporRequests.removeValue(forKey: id)!
     }
 
     var baseUrl: URL {
@@ -27,18 +23,37 @@ actor MockServer {
         return components.url!
     }
 
-    private func requestHandler(_ req: Request) async -> Response {
-        let testId = UUID().uuidString
-        vaporRequests[testId] = req
-        return Response(
-            status: .ok,
-            version: .http1_1,
-            headers: [vaporRequestHeader: testId],
-            body: "Success"
-        )
+    func getVaporRequest(_ response: URLResponse) -> Request {
+        middleware.getVaporRequest(response)
+    }
+
+    private func requestHandler(_: Request) async -> String {
+        "Success"
     }
 
     deinit {
         app.shutdown()
+    }
+}
+
+private class MockServerMiddleware: Middleware, @unchecked Sendable {
+    init() {}
+
+    private let headerName = "X-Vapor-Test-ID"
+    private var requests: [String: Request] = [:]
+
+    func getVaporRequest(_ response: URLResponse) -> Request {
+        let id = (response as! HTTPURLResponse).value(forHTTPHeaderField: headerName)!
+        return requests.removeValue(forKey: id)!
+    }
+
+    func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
+        let testId = UUID().uuidString
+        requests[testId] = request
+
+        return next.respond(to: request).map { response in
+            response.headers.add(name: self.headerName, value: testId)
+            return response
+        }
     }
 }
